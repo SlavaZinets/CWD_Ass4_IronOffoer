3b43bddc042325311af21d4a8b5d5886a4adab6f
Maksym926 <maxchechotkin@gmail.com>
2025-05-06 13:41:11 +0100

    Add basic account layout

---
+++ src/data/accounts.js
@@ -0,0 +1,12 @@
+export const accounts =[
+    {
+        
+        name: "Maksym",
+        lastName: "Chechotkin",
+        balance: 1000,
+        dateOfBirth: "25/05/2007",
+        email: "maxchechotkin@gmail.com",
+        phoneNumber: "0964151541",
+        dateCreated: "20/12/2011"
+    }
+]
