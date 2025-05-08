1245f879190bb1067223c3eb4b9d7b17da23611a
Maksym926 <maxchechotkin@gmail.com>
2025-05-08 22:30:07 +0100

    Add styles and implement avatar select menu

--- src/data/accounts.js
+++ src/data/accounts.js
@@ -6,6 +6,6 @@ export let accounts =[
         dateOfBirth: "25/05/2007",
         email: "maxchechotkin@gmail.com",
         phoneNumber: "0964151541",
-        dateCreated: "20/12/2011"
+        dateModified: "20/12/2011"
     }
 ]
